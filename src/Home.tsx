8db0063adec43fd3164430bab650b637e779a3ae
Kaxramonovravshan <r17ravshan@gmail.com>
2025-08-02 11:13:13 +0500

    aaa

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
 import { auth } from "./firebase.config";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = () => {
   const [currentUser, setCurrentUser] = useState<null | string>("");
 
   useEffect(() => {
-    const user = auth.currentUser;
-    if (user) {
-      setCurrentUser(user.email);
-    }
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user.email);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    return () => unsubscribe();
   }, []);
 
   return (
     <div>
-      <h1 className="text-3xl">Hello - {currentUser}</h1>
+      <h1 className="text-3xl">Hello - {currentUser || "Guest"}</h1>
     </div>
   );
 };
